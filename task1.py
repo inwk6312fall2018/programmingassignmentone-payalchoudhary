a20859a2a8229341c00c395d13a47b2a0cc52d2c
payalchoudhary <py492152@dal.ca>
2018-10-01 07:06:59 -0700

    task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,16 @@
-''
+#Task1
+
+file = open("running-config.cfg")
+mylist_intname = []
+#find the interface
+for line in file:
+	#remove whitespaces
+	line= line.strip()
+	#list of string for each line
+	line= line.split("!")
+	print (line)
+	""" check list of 0 is "interface" and copy list of 1"""
+	if (line[0] == "interface"):
+		mylist_intname.append(line[1])
+	
+#print(mylist_intname)
