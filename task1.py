984b63b4664b75b47816d76df1cad7d849924d9b
payalchoudhary <py492152@dal.ca>
2018-10-01 07:32:51 -0700

    Created a list of namif

--- task1.py
+++ task1.py
@@ -1,16 +1,21 @@
 #Task1
+import string
 
 file = open("running-config.cfg")
 mylist_intname = []
-#find the interface
+mylist_nameif = []
+""" find the interface and store it in list "mylist_intname" """
 for line in file:
 	#remove whitespaces
-	line= line.strip()
+	line= line.strip(string.punctuation and string.whitespace)
 	#list of string for each line
-	line= line.split("!")
-	print (line)
-	""" check list of 0 is "interface" and copy list of 1"""
+	line= line.split()
+#	print (line)
+	""" check if list of 0 is "interface" and copy list of 1"""
 	if (line[0] == "interface"):
 		mylist_intname.append(line[1])
-	
+	""" check if list of 0 is "nameif" and copy list of 1"""
+	if (line[0] == "nameif"):
+		mylist_nameif.append(line[1])	
 #print(mylist_intname)
+print(mylist_nameif)
