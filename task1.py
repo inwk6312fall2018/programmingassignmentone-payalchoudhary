f0e8e559e5a528ecfb4b87c92791b3891c0201b3
PayalChoudhary <py492152@dal.ca>
2018-10-02 20:59:12 -0300

    created tuple

--- task1.py
+++ task1.py
@@ -1,21 +1,36 @@
 #Task1
+
 import string
 
-file = open("running-config.cfg")
-mylist_intname = []
-mylist_nameif = []
-""" find the interface and store it in list "mylist_intname" """
-for line in file:
+def intname():	
+	file = open("running-config.cfg")
+	mylist_intname = []	
+	""" find the interface and store it in list "mylist_intname" """
+	for line in file:
 	#remove whitespaces
-	line= line.strip(string.punctuation and string.whitespace)
-	#list of string for each line
-	line= line.split()
-#	print (line)
-	""" check if list of 0 is "interface" and copy list of 1"""
-	if (line[0] == "interface"):
-		mylist_intname.append(line[1])
-	""" check if list of 0 is "nameif" and copy list of 1"""
-	if (line[0] == "nameif"):
-		mylist_nameif.append(line[1])	
-#print(mylist_intname)
-print(mylist_nameif)
+		line= line.strip(string.punctuation and string.whitespace)
+		line= line.split()
+		if (line[0] == "interface"):
+			mylist_intname.append(line[1])
+	return (mylist_intname)
+
+def nameif():	
+	file = open("running-config.cfg")	
+	mylist_nameif = []
+	'''find the nameif and store it in mylist_nameif'''
+	for line in file:
+	#remove whitespaces
+		line= line.strip(string.punctuation and string.whitespace)
+		line= line.split()
+#		print(line)
+		word = line[len(line)-1]
+		if(line[0] == "nameif"):
+			mylist_nameif.append(line[1])
+		if (word == "nameif"):
+			mylist_nameif.append("no nameif")
+	return (mylist_nameif)	
+
+int_name = intname()
+name_if = nameif()
+tuple_int_nameif = zip(int_name,name_if)
+print(list(tuple_int_nameif))
